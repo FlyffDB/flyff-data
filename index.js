bb1a41385b304dddfc60db92295c32d2ec6b5af1
Adam <axfanwashere@gmail.com>
2021-03-23 23:38:57 -0700

    update exports and add .npmrc

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
+const fs = require('fs');
+const path = require('path')
 
-const adapter = new FileSync('db.json')
+const adapter = new FileSync(path.join(__dirname, 'db.json'));
 const db = low(adapter)
-
 
 const classes = db.get('classes');
 const npcs = db.get('npcs');
 
-export {
+module.exports = {
   classes,
   npcs
 }
